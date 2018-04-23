6fd9674b2c19920e8d7fbe83c900b9afae1c35d3
Michael Gates <michaelgatesdev@gmail.com>
2018-04-23 12:34:57 -0400

    Renamed package name for consistency

---
+++ src/main/java/com/michaelgatesdev/ExifExplorer/photo/criteria/datetime/AfterDateTimeCriteria.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) Michael Gates (MichaelGatesDev@gmail.com) 2018
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+package com.michaelgatesdev.ExifExplorer.photo.criteria.datetime;
+
+import java.time.LocalDateTime;
+
+public class AfterDateTimeCriteria extends DateTimeCriteria
+{
+    public AfterDateTimeCriteria(LocalDateTime ldt)
+    {
+        super(DateTimeCriteriaCondition.AFTER, new LocalDateTime[]{ ldt });
+    }
+    
+    
+    @Override
+    public boolean compare(LocalDateTime ldt)
+    {
+        return ldt.isAfter(this.getDates()[0]);
+    }
+}
